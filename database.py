b89c9f7ef597a893bda35ee34e0eb1d34b130a1f
Rathoreshivamsingh <you@example.com>
2024-08-23 10:28:17 +0530

    Production database change

--- database.py
+++ database.py
@@ -2,14 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://todo_app_ehle_user:WPwJUfIWSGXYrIITqqdJeXMM17qzNObd@dpg-cr41b5rv2p9s73cjuqgg-a.oregon-postgres.render.com/todo_app_ehle'
 
 # MYSQL Series
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+    SQLALCHEMY_DATABASE_URL )
 
 # MYSQL Series
 # engine = create_engine(
